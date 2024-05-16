85c32facc9c67daf129b79348ae3147a9b28e44d
Molliet <49567548+Molliet@users.noreply.github.com>
2024-05-16 19:46:28 -0300

    Add files via upload

--- HandTracker.py
+++ HandTracker.py
@@ -1,6 +1,13 @@
 import cv2
+import cvzone
+from cvzone.ColorModule import ColorFinder
 import mediapipe as mp
 import numpy as np
+
+cx=0
+cy=0
+hcx=0
+hcy=0
 
 
 #Lining Landmarks
@@ -10,14 +17,33 @@ mp_drawing_styles = mp.solutions.drawing_styles
 #var for function
 mphands = mp.solutions.hands
 
+#Color Finder
+myColorFinder = ColorFinder(False)
 
 #Video Capture
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture('bball.mp4')
 
 hands = mphands.Hands()
+hsvVals = {'hmin': 0, 'smin': 164, 'vmin': 120, 'hmax': 9, 'smax': 213, 'vmax': 155}
+
+
+
 
 while True:
     data, image = cap.read()
+    image_height, image_width, _ = image.shape
+    imgClean = image
+
+
+    #find and draw ball
+    imgColor, Mask = myColorFinder.update(image,hsvVals)
+    image , contours = cvzone.findContours(image, Mask, minArea=500)
+    if contours:
+        cx, cy = contours[0]['center']
+
+
+
+    #find and draw hand
     #flip image
     image = cv2.cvtColor(cv2.flip(image,1), cv2.COLOR_BGR2RGB)
     #store results
@@ -31,5 +57,17 @@ while True:
                 image,
                 hand_landmarks, mphands.HAND_CONNECTIONS
             )
-    cv2.imshow('HandTracker', image)
+            for ids, landmrk in enumerate(hand_landmarks.landmark):
+                if ids == 9:
+                    hcx, hcy = landmrk.x * image_width, landmrk.y*image_height
+
+
+    
+
+    print(hcx, hcy, cx,cy)
+
+    cv2.imshow('Tracking', image)
+    cv2.imshow('Clean', imgClean)
     cv2.waitKey(1)
+
+    
